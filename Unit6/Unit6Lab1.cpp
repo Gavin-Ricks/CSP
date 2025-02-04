014fdd85d1113d2d3d3d461a95a8da1f3844dda1
Gavin-Ricks <gavin.ricks2020@gmail.com>
2025-02-04 23:00:14 +0000

    1 Error 2 fix

--- Unit6/Unit6Lab1.cpp
+++ Unit6/Unit6Lab1.cpp
@@ -1,5 +1,6 @@
 //Add the appropriate preprocessor directives here.
 #include <iostream>
+#include <random>
 #include "conio.h"
 
  
@@ -8,6 +9,7 @@
 void initialize(char f[][25]);
 void move(char f[][25], char dir);
 void display(char f[][25]);
+void placeRock(char f[][25]);
 
 
 
@@ -72,20 +74,15 @@ void initialize(char f[][25])
         }
     }
     f[tr][tc] = 'O';
+    placeRock(f);
 }
 
  
 
 void display(char f[][25])
 {
-    for(int row = 0; row < 20; row++){
-        for(int col = 0; col < 25; col++){
-            if(f[row][col] == 'O'){
-                cout << "\tCurrent Location: " << row <<  ", " << col << endl;
-                break;
-            }
-        }
-    }
+    system("clear");
+    cout << "\tCurrent Location: " << tr <<  ", " << tc << endl;
     cout << "\t===========================" << endl;
     for (int row = 0; row < 20; row++){
         cout << "\t|";
@@ -97,56 +94,81 @@ void display(char f[][25])
     cout << "\t===========================" << endl;
 }
 
- 
 
 void move(char f[][25], char dir)
 {
-    //Will Change before checking if it's possible to move in that direction, change this code so that it would replace O with * in the switch statement...
-    //Finding the 'O' Value
-    for(int row = 0; row < 20; row++){
-        for(int col = 0; col < 25; col++){
-            if(f[row][col] == 'O'){
-                f[row][col] = '*'; //Replacing 'O' with '*'
+    switch (dir){
+        case '8':
+        // Move up
+        if(tr > 0){
+            if(f[tr-1][tc] == 'X'){
                 break;
             }
-        }
-    }
-    
-    //Direction
-    switch (dir){
-        case 8:
-        //This is up
-        if(tr > 0){
+            f[tr][tc] = '*';
             tr = tr - 1;
             f[tr][tc] = 'O';
         }
-        
         break;
-        case 2:
-        //This is down
-        if(tr < 20){
+        case '2':
+        // Move down
+        if(tr < 19){
+            if(f[tr+1][tc] == 'X'){
+                break;
+            }
+            f[tr][tc] = '*';
             tr = tr + 1;
             f[tr][tc] = 'O';
         }
         break;
-        case 4:
-        //This is left
+        case '4':
+        // Move left
         if(tc > 0){
+            if(f[tr][tc-1] == 'X'){
+                break;
+            }
+            f[tr][tc] = '*';
             tc = tc - 1;
             f[tr][tc] = 'O';
         }
         break;
-        case 6:
-        //This is right
-        if(tc < 25){
+        case '6':
+        // Move right
+        if(tc < 24){
+            if(f[tr][tc+1] == 'X'){
+                break;
+            }
+            f[tr][tc] = '*';
             tc = tc + 1;
             f[tr][tc] = 'O';
         }
         break;
-        default:
-        //nothing
-        break;
     }
     display(f);
+}   
 
+void placeRock(char f[][25])
+{
+    int rocksPlaced = 0;
+
+    //Place first rock
+    int row = rand() % 17 + 3;
+    int col = rand() % 22 + 3;
+    f[row][col] = 'X';
+    f[row+1][col] = 'X';
+    f[row][col+1] = 'X';
+    f[row+1][col+1] = 'X';
+    rocksPlaced++;
+
+    //Place the rest of the rocks
+    do{
+        row = rand() % 15 + 3;
+        col = rand() % 20 + 3;
+        if(f[row][col] == ' ' && f[row+1][col] == ' ' && f[row][col+1] == ' ' && f[row+1][col+1] == ' '){
+            f[row][col] = 'X';
+            f[row+1][col] = 'X';
+            f[row][col+1] = 'X';
+            f[row+1][col+1] = 'X';
+            rocksPlaced++;
+        }
+    } while(rocksPlaced < 3);
 }
